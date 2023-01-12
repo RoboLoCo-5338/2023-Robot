e7e3a8236ac766b9889ef9095a68dee0996c8a66
abbymartin <abbyvmartin51@gmail.com>
2023-01-12 18:17:37 -0500

    add tank drive code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,11 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -21,16 +24,26 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Drivetrain drivetrain = new Drivetrain();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  // controllers
+  private static Joystick controller1 = new Joystick(0); //driver
+  private static Joystick controller2 = new Joystick(1); //operator
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+    configureDefaultCommands();
   }
+
+  public Command defaultDrive = new RunCommand(
+      () -> drivetrain.tankDrive(
+        controller1.getRawAxis(1),
+        controller1.getRawAxis(5)
+      ),
+      drivetrain
+    );
 
   /**
    * Use this method to define your trigger->command mappings. Triggers can be created via the
@@ -48,7 +61,11 @@ public class RobotContainer {
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    //m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  }
+
+  private void configureDefaultCommands() {
+    drivetrain.setDefaultCommand(defaultDrive);
   }
 
   /**
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,10 +5,31 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax; 
+import frc.robot.Constants;
 
 public class Drivetrain extends SubsystemBase {
+  
+  private CANSparkMax leftFront;
+  private CANSparkMax leftRear;
+  private CANSparkMax rightFront;
+  private CANSparkMax rightRear;
+
   /** Creates a new Drivetrain. */
-  public Drivetrain() {}
+  public Drivetrain() {
+    leftFront = new CANSparkMax(Constants.MOTOR_ID_0, null);
+    leftRear = new CANSparkMax(Constants.MOTOR_ID_1, null);
+    leftRear.follow(leftFront);
+
+    rightFront = new CANSparkMax(Constants.MOTOR_ID_2, null);
+    rightRear = new CANSparkMax(Constants.MOTOR_ID_3, null);
+    rightRear.follow(rightFront);
+  }
+
+  public void tankDrive(double left, double right) {
+    leftFront.set(left);
+    rightFront.set(-right);
+  }
 
   @Override
   public void periodic() {
