c17fe97c729b5c76b8c1c667486e98ba29dfab9e
aedan07 <40202903+aedan07@users.noreply.github.com>
2023-01-14 13:29:16 -0500

    Added PID config

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,6 +4,10 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import frc.robot.Direction;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -12,6 +16,16 @@ public final class Autos {
   /** Example static factory for an autonomous command. */
   public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
     return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+  }
+
+  public static CommandBase driveDistanceCommand(double distance, Direction direction) {
+    return new FunctionalCommand(
+            () -> RobotContainer.drivetrain.resetPosition(),
+            () -> RobotContainer.drivetrain.driveDistance(distance, direction),
+            (interrupt) -> RobotContainer.drivetrain.tankDrive(0, 0),
+            () -> Math.abs(RobotContainer.drivetrain.getPosition()) >= Math.abs(Drivetrain.targetPosition) - 1000,
+            RobotContainer.drivetrain
+    );
   }
 
   private Autos() {
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,15 +5,38 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMax; 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
+
 import frc.robot.Constants;
+import frc.robot.Direction;
 
 public class Drivetrain extends SubsystemBase {
-  
+  // TODO Placeholder constants.
+  private static final double TICKS_PER_REVOLUTION = 4096;
+  private static final double WHEEL_DIAMETER = 6.0;
+  private static final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
+  private static final double GEAR_RATIO = 10.7 / 1;
+  private static final double TICKS_PER_INCH = (TICKS_PER_REVOLUTION / WHEEL_CIRCUMFERENCE);
+
+  // PID values for teleop.
+  public static final double VELOCITY_P = 0.0110;
+  public static final double VELOCITY_I = 0.0;
+  public static final double VELOCITY_D = 0.0;
+  public static final double VELOCITY_FEED_FORWARD = 0.0;
+
+  // PID values for autonomous.
+  public static final double POSITION_P = 0.0175821;
+  public static final double POSITION_I = 0.0;
+  public static final double POSITION_D = 0.0020951;
+  public static final double POSITION_FEED_FORWARD = 0.0;
+
   private CANSparkMax leftFront;
   private CANSparkMax leftRear;
   private CANSparkMax rightFront;
   private CANSparkMax rightRear;
+  public static double targetPosition;
+  public static Direction targetDirection;
 
   /** Creates a new Drivetrain. */
   public Drivetrain() {
@@ -31,9 +54,52 @@ public class Drivetrain extends SubsystemBase {
     rightFront.set(-right);
   }
 
-  public void driveDistance(double distance) {
-    
+  public void driveDistance(double inches, Direction direction) {
+    targetDirection = direction;
+    if (direction == Direction.FORWARD) {
+      targetPosition = -inches * TICKS_PER_INCH * GEAR_RATIO;
+    } else if (direction == Direction.BACKWARD) {
+      targetPosition = inches * TICKS_PER_INCH * GEAR_RATIO;
+    } else {
+      targetPosition = 0;
+    }
+
+//    leftFront.set(ControlMode.Position, targetPosition);
+//    rightFront.set(ControlMode.Position, targetPosition);
+    tankDrive(targetPosition, targetPosition);
   }
+
+  public void setPID(double kP, double kI, double kD, double kF) {
+    SparkMaxPIDController rightFrontPID = rightFront.getPIDController();
+    rightFrontPID.setP(kP);
+    rightFrontPID.setI(kI);
+    rightFrontPID.setD(kD);
+    rightFrontPID.setFF(kF);
+    rightFront.setCANTimeout(100);
+
+    SparkMaxPIDController leftFrontPID = leftFront.getPIDController();
+    leftFrontPID.setP(kP);
+    leftFrontPID.setI(kI);
+    leftFrontPID.setD(kD);
+    leftFrontPID.setFF(kF);
+    leftFront.setCANTimeout(100);
+
+    SparkMaxPIDController rightRearPID = rightRear.getPIDController();
+    rightRearPID.setP(kP);
+    rightRearPID.setI(kI);
+    rightRearPID.setD(kD);
+    rightFrontPID.setFF(kF);
+    rightFront.setCANTimeout(100);
+
+    SparkMaxPIDController leftRearPID = leftRear.getPIDController();
+    leftRearPID.setP(kP);
+    leftRearPID.setI(kI);
+    leftRearPID.setD(kD);
+    leftRearPID.setFF(kF);
+    leftRear.setCANTimeout(100);
+  }
+
+
 
   @Override
   public void periodic() {
