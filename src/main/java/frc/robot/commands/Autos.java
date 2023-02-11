af9184789db35a881e7ca6ff40519d8989b335ec
abbymartin <abbyvmartin51@gmail.com>
2023-02-11 13:41:24 -0500

    remove example command & subsystem

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -10,15 +10,10 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.Direction;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public final class Autos {
-  /** Example static factory for an autonomous command. */
-  public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
-  }
 
   public static CommandBase angleTurnCommand(double angle, Direction direction) {
 		return new FunctionalCommand(
