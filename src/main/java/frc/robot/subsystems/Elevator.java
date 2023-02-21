25ae86aa1758ba328c852779c6bfb627459c6a4b
Meeebis <1038221@lcps.org>
2023-02-21 18:00:19 -0500

    updated elevator.java but probably not needed

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -8,6 +8,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import frc.robot.Constants;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
 
 public class Elevator extends SubsystemBase {
   private CANSparkMax elevatorMotor;
@@ -17,9 +19,26 @@ public class Elevator extends SubsystemBase {
       // elevatorMotor.set(preset);
     }
 
-    public void setHeight(int h) {
-      elevatorMotor.set(h);
-      // TODO 2/18 (photo sent in email)
+    public void setHeight(int preset) {
+      double current = elevatorMotor.getEncoder().getPosition();
+      double change = height[preset] - current;
+      current = height[preset];
+      elevatorMotor.getPIDController().setReference(change, ControlType.kPosition);
+      elevatorMotor.getEncoder().setPosition(0);
+      // TODO 2/18
     }
+    //private void configController(CANSparkMax sparkMax, double kP, double kI, double kD, double kF)
+    {
+      
+      //SparkMaxPIDController controller = sparkMax.getPIDController();
+      //controller.setP(kP);
+      //controller.setI(kI);
+      //controller.setD(kD);
+      //controller.setFF(kF);
+      //controller.setOutputRange(-peakOutput, peakOutput);
+      //sparkMax.setCANTimeout(100);
+      //TO DO pid later
+    
+   
 }
 
