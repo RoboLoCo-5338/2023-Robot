53e20fafb0f0dc9febe24453b9f5a1cfcbdacefd
GIDEON BAUER (836902) <gideonbauer999@gmail.com>
2023-02-21 18:42:39 -0500

    Revert "Todo"
    This reverts commit a0c84ecc7cd28a4b827925e6a044513e0a93c017.

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -14,12 +14,11 @@ public class Elevator extends SubsystemBase {
 
   public Elevator(int preset) {
       elevatorMotor = new CANSparkMax(Constants.MOTOR_ID_4, MotorType.kBrushless);
-      // elevatorMotor.set(preset);
+      elevatorMotor.set(preset);
     }
 
     public void setHeight(int h) {
-      elevatorMotor.set(h);
-      // TODO 2/18
+      elevatorMotor.set(g);
     }
 }
 
