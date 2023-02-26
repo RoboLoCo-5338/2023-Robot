4d93911eb2b6d2a277e2f4ba560bffc6bcfc6d9a
mooshroom_person <nithya.vinodh14@gmail.com>
2023-02-25 22:15:05 -0500

    jank LED code (possibly)

---
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -0,0 +1,55 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.LED;//need to make a constant for the LED
+
+public class LEDSubsystem extends SubsystemBase{
+    private final AddressableLED m_led = new AddressableLED(LED.PWMPORT);
+    private final AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(LED.BUFFERSIZE);
+    private int m_rainbowFirstPixelHue;
+    private Elevator elevator;
+    
+    public LEDSubsystem(Elevator elevator){
+    this.elevator = ev;
+    m_led.setLength(m_ledBuffer.getLength());
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+    }
+
+    @Override
+    public void periodic(){
+        setLEDs();
+    }
+
+    private void roboLocoColor(){
+      setFrontAll(Color.kYellow); 
+    }
+
+    private void setLEDs() {
+      m_led.setData(m_ledBuffer);
+    }
+
+    private void setAll(Color color) {
+      for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+        m_ledBuffer.setLED(i, color);
+      }
+    }
+}
+
+
+  
+  
+
+  
+
+   
+  
+   
+    
+  
+    
+  
+
