01971c4ede8022bbf80d209cda8ec680ff1e39bf
aedanmills <40202903+aedanmills@users.noreply.github.com>
2023-03-06 11:24:18 -0500

    Update Drivetrain.java

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -39,6 +39,49 @@ public class Drivetrain extends SubsystemBase {
     
   }
 
+  public void driveDistance(double inches, Direction direction) {
+    targetDirection = direction;
+    if (direction == Direction.FORWARD) {
+      targetPosition = -inches * TICKS_PER_INCH * GEAR_RATIO;
+    } else if (direction == Direction.BACKWARD) {
+      targetPosition = inches * TICKS_PER_INCH * GEAR_RATIO;
+    } else {
+      targetPosition = 0;
+    }
+
+//    leftFront.set(ControlMode.Position, targetPosition);
+//    rightFront.set(ControlMode.Position, targetPosition);
+    tankDrive(targetPosition, targetPosition);
+  }
+/* 
+  public void setPID(double kP, double kI, double kD, double kF) {
+    SparkMaxPIDController rightFrontPID = rightFront.getPIDController();
+    rightFrontPID.setP(kP);
+    rightFrontPID.setI(kI);
+    rightFrontPID.setD(kD);
+    rightFrontPID.setFF(kF);
+    rightFront.setCANTimeout(100);
+    SparkMaxPIDController leftFrontPID = leftFront.getPIDController();
+    leftFrontPID.setP(kP);
+    leftFrontPID.setI(kI);
+    leftFrontPID.setD(kD);
+    leftFrontPID.setFF(kF);
+    leftFront.setCANTimeout(100);
+    SparkMaxPIDController rightRearPID = rightRear.getPIDController();
+    rightRearPID.setP(kP);
+    rightRearPID.setI(kI);
+    rightRearPID.setD(kD);
+    rightFrontPID.setFF(kF);
+    rightFront.setCANTimeout(100);
+    SparkMaxPIDController leftRearPID = leftRear.getPIDController();
+    leftRearPID.setP(kP);
+    leftRearPID.setI(kI);
+    leftRearPID.setD(kD);
+    leftRearPID.setFF(kF);
+    leftRear.setCANTimeout(100);
+  }
+*/
+
 
   @Override
   public void periodic() {
